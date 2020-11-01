c83fd26f045ab3da738da2f83713f44cba3efb54
CiechanM <m.ciechan12@gmail.com>
2020-11-01 14:15:35 +0100

    Rozpoczecie projektu

---
+++ IO_Lab2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IO_Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
